6e95e761fb20cef761fbf78be7ea27c791b53440
theBreadCompany <40942207+theBreadCompany@users.noreply.github.com>
2021-09-03 21:55:48 +0200

    update manifest lol

--- Package.swift
+++ Package.swift
@@ -1,13 +1,12 @@
 // swift-tools-version:5.2.0
-import PackageDescription
 
 let package = Package(
   name: "WKZombieRevised",
-  targets: [
-      Target(name: "WKZombieRevised"),
-      Target(name: "Example", dependencies:["WKZombieRevised"])
-  ],
   dependencies: [
-	   .Package(url: "https://github.com/mkoehnke/hpple.git", Version(0,2,2))
+	   .package(url: "https://github.com/mkoehnke/hpple.git", .branch("spm"))
+  ],
+  targets: [
+      .target(name: "WKZombieRevised"),
+      .target(name: "Example", dependencies:["WKZombieRevised"])
   ]
 )
